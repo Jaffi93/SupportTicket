3bf4996cbdb67235792902289e33712488997b08
mohamed <mohamedjaffi.93@gmail.com>
2023-05-20 21:02:09 +0400

    User backend completed such as routes, controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('started...');
+const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+//Connect to database
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hello' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started at this ${PORT} PORT`))
